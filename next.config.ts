0eee3803873043dc646ce9135017f9563bebad67
nylixin <147104517+nylixin@users.noreply.github.com>
2026-05-29 17:37:05 -0700

    Update next.config.ts
    vercel proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.57.40.22:5050/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
