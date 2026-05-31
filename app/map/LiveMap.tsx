d3f5bedc73c8ca173932af8437adb25fb52b0697
nylixin <147104517+nylixin@users.noreply.github.com>
2026-05-30 19:59:01 -0700

    gps location

--- app/map/LiveMap.tsx
+++ app/map/LiveMap.tsx
@@ -1,40 +1,40 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import { MapContainer, TileLayer, CircleMarker, Popup } from "react-leaflet";
 
 export default function LiveMap() {
-  const start: [number, number] = [33.6405, -117.8443];
-  const stop: [number, number] = [33.6587, -117.8265];
-  const destination: [number, number] = [33.6846, -117.8265];
+  const [position, setPosition] = useState<[number, number] | null>(null);
 
-  const route: [number, number][] = [start, stop, destination];
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition([
+          pos.coords.latitude,
+          pos.coords.longitude,
+        ]);
+      },
+      (err) => console.error(err)
+    );
+  }, []);
+
+  if (!position) {
+    return <div>Getting your location...</div>;
+  }
 
   return (
     <main className="h-screen w-screen">
       <MapContainer
-        center={start}
-        zoom={13}
-        scrollWheelZoom={true}
+        center={position}
+        zoom={15}
         style={{ height: "100vh", width: "100vw" }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-        <Marker position={start}>
-          <Popup>Start: UCI</Popup>
-        </Marker>
-
-        <Marker position={stop}>
-          <Popup>Transit Stop</Popup>
-        </Marker>
-
-        <Marker position={destination}>
-          <Popup>Destination</Popup>
-        </Marker>
-
-        <Polyline positions={route} />
+        <CircleMarker center={position} radius={10}>
+          <Popup>You are here</Popup>
+        </CircleMarker>
       </MapContainer>
     </main>
   );
